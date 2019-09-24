498ad10c9074f9fd01bf0860d85f522ae654e088
David Markham <davidmarkham76@gmail.com>
2019-09-24 14:31:39 +0100

    Add files via upload

--- sunday.py
+++ sunday.py
@@ -2,8 +2,22 @@
 # Is it Sunday?
 
 import datetime
+import calendar
+print("Is today Sunday?")
 
 if datetime.datetime.today().weekday() == 6:
   print("Yay! It is Sunday.")
 else:
   print("Unfortunately it is not Sunday.")
+
+def findDay(date): 
+    day, month, year = (int(i) for i in date.split(' '))     
+    dayNumber = calendar.weekday(year, month, day) 
+    days =["Monday", "Tuesday", "Wednesday", "Thursday", 
+                         "Friday", "Saturday", "Sunday"] 
+    return (days[dayNumber]) 
+  
+# Driver program 
+print("Today is:")
+date = '24 09 2019'
+print(findDay(date))
